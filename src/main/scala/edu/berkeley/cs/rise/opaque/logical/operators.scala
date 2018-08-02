528a6427981158e4cd9386ea0c692710e5e46240
muye <jason.wwx@antfin.com>
2018-08-02 19:00:22 +0800

    Remove unused code

--- src/main/scala/edu/berkeley/cs/rise/opaque/logical/operators.scala
+++ src/main/scala/edu/berkeley/cs/rise/opaque/logical/operators.scala
@@ -84,11 +84,6 @@ case class EncryptedLocalRelation(
   }
 }
 
-case class EncryptedMetastoreRelation()
-  extends OpaqueOperator with MultiInstanceRelation {
-
-}
-
 case class EncryptedBlockRDD(
     output: Seq[Attribute],
     rdd: RDD[Block],
